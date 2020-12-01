f04454363e1ba6aa4e5cfac0b74f3d191280ca37
Tetiana Proshyna <mollinne2@gmail.com>
2020-12-02 01:05:33 +0200

    Add tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,16 @@
-// Напиши фукцнию findLongestWord(string), которая принимает параметром произвольную строку (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
+const findLongestWord = function (string) {
+  let words = string.split(' ');
+  let longestWord = words[0];
+  for (let i = 1; i <= words.length - 1; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }
+  }
+  return longestWord;
+};
 
-// const findLongestWord = function(string) {
-//   // твой код
-// };
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
 
-// /*
-//  * Вызовы функции для проверки работоспособности твоей реализации.
-//  */
-// console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+console.log(findLongestWord('Google do a roll')); // 'Google'
 
-// console.log(findLongestWord('Google do a roll')); // 'Google'
-
-// console.log(findLongestWord('May the force be with you')); // 'force'
+console.log(findLongestWord('May the force be with you')); // 'force'
--- js/task-4.js
+++ js/task-4.js
@@ -1,26 +1,16 @@
-// Напиши функцию formatString(string) которая принимает строку и форматирует ее если необходимо.
+const formatString = function (string) {
+  if (string.length <= 40) {
+    return string;
+  }
+  let stringFormated = string.substr(0, 40).concat('...');
+  return stringFormated;
+};
 
-// Если длина строки не превышает 40 символов, функция возвращает ее в исходном виде.
-// Если длина больше 40 символов, то функция обрезает строку до 40-ка символов и добавляет в конец строки троеточие '...', после чего возвращает укороченную версию.
-// const formatString = function(string) {
-//   // твой код
-// };
-
-// /*
-//  * Вызовы функции для проверки работоспособности твоей реализации.
-//  */
-// console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
-// // вернется оригинальная строка
-
-// console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
-// // вернется форматированная строка
-
-// console.log(formatString('Curabitur ligula sapien.'));
-// // вернется оригинальная строка
-
-// console.log(
-//   formatString(
-//     'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
-//   ),
-// );
-// // вернется форматированная строка
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+console.log(formatString('Curabitur ligula sapien.'));
+console.log(
+  formatString(
+    'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+  ),
+);
